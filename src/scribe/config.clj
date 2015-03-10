73f0531f01d9030a02d5ba79cbf169d061050a43
Vincent Rivellino <var@promotably.com>
2015-03-10 14:29:16 -0400

    Loggly tags

--- src/scribe/config.clj
+++ src/scribe/config.clj
@@ -30,7 +30,7 @@
 
 (def loggly-url
   (or (get-config-value "LOGGLY_URL")
-      "http://logs-01.loggly.com/inputs/2032adee-6213-469d-ba58-74993611570a/tag/http/"))
+      "http://logs-01.loggly.com/inputs/2032adee-6213-469d-ba58-74993611570a/tag/dev,scribe/"))
 
 (defn- get-database-config
   "Checks environment variables for database config settings. These
